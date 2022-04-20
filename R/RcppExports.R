72323976d4ed9d0942fd6c64d402c9cc39373dc8
Dongjun Chung <dongjun.chung@gmail.com>
2022-04-20 11:56:54 -0400

    Fixed the Rcpp call error

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,10 +2,10 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 Gibbs_e_it_ann <- function(beta_mat, G_mat, n_SNP, n_GibbsStep, true_Gamma, A_mat) {
-    .Call('_GGPAannot_Gibbs_e_it_ann', PACKAGE = 'GGPA2', beta_mat, G_mat, n_SNP, n_GibbsStep, true_Gamma, A_mat)
+    .Call('_GGPA2_Gibbs_e_it_ann', PACKAGE = 'GGPA2', beta_mat, G_mat, n_SNP, n_GibbsStep, true_Gamma, A_mat)
 }
 
 Gibbs_e_it_no_ann <- function(beta_mat, G_mat, n_SNP, n_GibbsStep) {
-    .Call('_GGPAannot_Gibbs_e_it_no_ann', PACKAGE = 'GGPA2', beta_mat, G_mat, n_SNP, n_GibbsStep)
+    .Call('_GGPA2_Gibbs_e_it_no_ann', PACKAGE = 'GGPA2', beta_mat, G_mat, n_SNP, n_GibbsStep)
 }
 
